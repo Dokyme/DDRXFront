a8dfc443fba8c103697ce087a291e1c400a33ddb
Dokyme <zdkscope@qq.com>
2018-03-14 22:12:52 +0800

    Finished drawer opening and dragging

--- app/src/main/java/com/ddrx/ddrxfront/MainActivity.kt
+++ app/src/main/java/com/ddrx/ddrxfront/MainActivity.kt
@@ -1,12 +1,52 @@
 package com.ddrx.ddrxfront
 
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mDrawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawer_list.adapter = ArrayAdapter(this, R.layout.drawer_list_item, listOf("Hello", "World", "My", "Friend"))
+        var actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeButtonEnabled(true)
+        }
+
+        mDrawerToggle = ActionBarDrawerToggle(this, drawer_layout, R.string.drawer_open, R.string.drawer_close)
+        drawer_layout.addDrawerListener(mDrawerToggle)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        mDrawerToggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        mDrawerToggle.onConfigurationChanged(newConfig)
     }
 }
